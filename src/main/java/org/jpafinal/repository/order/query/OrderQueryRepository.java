2ffbea47fe6d16a213df5263233b4af84af0f66f
halfdev <gusgodnd@naver.com>
2020-01-18 12:23:02 +0900

    ref #10 Dto Direct Select Collection 2

--- src/main/java/org/jpafinal/repository/order/query/OrderQueryRepository.java
+++ src/main/java/org/jpafinal/repository/order/query/OrderQueryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -43,4 +45,72 @@ public class OrderQueryRepository {
                         " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
+
+    /**
+     * @author halfdev
+     * @since 2020-01-18
+     * Query 를 한 번 날리고, Memory 에서 Map 으로 다 가져와서 result.forEach 에서 set 을 해준다.
+     */
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new org.jpafinal.dto.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        // orderId 를 기준으로 Map 으로 바꾼다. Key : getOrderId , Value : OrderItemQueryDto
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    /**
+    * @author halfdev
+    * @since 2020-01-18
+    * findAllByDto_optimization() Refactoring Version
+    */
+//    public List<OrderQueryDto> findAllByDto_optimizationV2() {
+//        List<OrderQueryDto> result = findOrders();
+//
+//        List<Long> orderIds = toOrderIds(result);
+//
+//        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+//
+//        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+//
+//        return result;
+//    }
+//
+//    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+//        List<OrderItemQueryDto> orderItems = em.createQuery(
+//                "select new org.jpafinal.dto.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+//                        " from OrderItem oi" +
+//                        " join oi.item i" +
+//                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+//                .setParameter("orderIds", orderIds)
+//                .getResultList();
+//
+//        // orderId 를 기준으로 Map 으로 바꾼다. Key : getOrderId , Value : OrderItemQueryDto
+//        return orderItems.stream()
+//                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+//    }
+//
+//    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+//        return result.stream()
+//                    .map(o -> o.getOrderId())
+//                    .collect(Collectors.toList());
+//    }
+
+
 }
